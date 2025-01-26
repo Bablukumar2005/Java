ca2b835f24dde9b8886971c906f45cd698b1c883
Bablukumar2005 <bablukumar030205@gmail.com>
2025-01-26 10:04:42 +0530

    Second program

---
+++ xyz.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class xyz{
+    public static void main(String ars[]){
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.printf("%d",a);
+    }
+}
