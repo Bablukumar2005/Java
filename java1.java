f58be7deadd8c82693c1d756821a96484a56f5da
Bablu Kumar <153607541+Bablukumar2005@users.noreply.github.com>
2025-01-26 10:22:07 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -3,7 +3,7 @@ class java1{
     public static void main(String ars[]){
         System.out.println("hello");
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        int a = sc.nextInt();//new variable
         int b=sc.nextInt();
         int c=a+b;
         System.out.println(c);
