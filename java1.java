d5dd4c7fbaf9f497ff6b91e9b590c1c8a1d822a0
Bablukumar2005 <bablukumar030205@gmail.com>
2025-01-26 10:19:10 +0530

    addition

---
+++ java1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class java1{
+    public static void main(String ars[]){
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+        System.out.printf("%d",a);
+    }
+}
