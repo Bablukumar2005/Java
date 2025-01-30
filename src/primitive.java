d29fa59da1fabe58f38cf19113742fb4b678eb99
Bablukumar2005 <bablukumar030205@gmail.com>
2025-01-30 08:11:38 +0530

    first commit

---
+++ src/primitive.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class primitive {
+    public static void main(String[] args) {
+        int rollno=64;
+        char letter='r';
+        float marks=98.67f;   /* by default decimal is double */
+        double largerDecimalNumbers=347552.43634;
+        long LargerInteger=456745678546L;
+        boolean check=false;
+        System.out.println(letter);
+//        Every thing Which is started with
+//        capital letter in java is a Class
+        Integer rno =64;//here Interger is a rapper class and rno in object
+       int a=23_000_000;
+
+    }
+}
