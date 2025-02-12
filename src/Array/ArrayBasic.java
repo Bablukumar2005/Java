13672f569e770190ed9b4ba77a0f0db140d39d4a
Bablukumar2005 <bablukumar030205@gmail.com>
2025-02-12 21:38:35 +0530

    Array0

---
+++ src/Array/ArrayBasic.java
@@ -0,0 +1,15 @@
+package src.Array;
+import java.util.Scanner;
+public class ArrayBasic {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println(arr[0]);
+        String []str= new String[2];//0 bez it is array of primitive where int is not equal to null
+        System.out.println(str[0]);//null it is array of object
+
+
+    }
+}
